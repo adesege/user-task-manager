2d30a65cdd99fd5f8badef0ddf356e49a9723ce2
Fadojutimi Temitayo Olusegun <Fadojutimitemitayo@gmail.com>
2019-12-13 08:37:26 +0100

    fix: make services.sh commands run in parallel

--- scripts/services.sh
+++ scripts/services.sh
@@ -1,9 +1,10 @@
-for service in ./services/*; do (
-  if [[ -f "$service"/package.json ]]; then
-    echo "==============================";
-    echo "Starting service \"$service\"";
-    echo "==============================";
-    ( cd $service; yarn dev )
-  fi
-)
+for service in ./services/*;
+  do (
+    if [[ -f "$service"/package.json ]]; then
+      echo "==============================";
+      echo "Starting service \"$service\"";
+      echo "==============================";
+      ( cd $service; yarn dev )
+    fi
+  ) &
 done
